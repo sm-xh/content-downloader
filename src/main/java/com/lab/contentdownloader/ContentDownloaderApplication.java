bf06c868dd6bc72a88591886b73656f78d534d22
Piotr Smach <p.smach@dicella.com>
2022-04-13 14:26:01 +0200

    project initalizr

---
+++ src/main/java/com/lab/contentdownloader/ContentDownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.lab.contentdownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentDownloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/contentdownloader/ContentDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.contentdownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
