6956cc0ef7c032217a5b4e6eca51945feba6f12d
Piotr Smach <p.smach@dicella.com>
2022-05-12 19:20:41 +0200

    heroku deployment

--- django_content_downloader/settings.py
+++ django_content_downloader/settings.py
@@ -114,8 +114,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
 
 # Default primary key field type
