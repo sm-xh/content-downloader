e72bb888636c849d327c3066173ad3b9ce339fc3
Piotr Smach <p.smach@dicella.com>
2022-05-12 21:22:07 +0200

    heroku deployment

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn django_content_downloader.wsgi; python manage.py collectstatic --noinput
+web: gunicorn django_content_downloader.wsgi
--- django_content_downloader/settings.py
+++ django_content_downloader/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = ()
 
