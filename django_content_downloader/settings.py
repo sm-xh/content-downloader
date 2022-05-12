923d9115a26e9117924391337962129adc7fae15
Piotr Smach <p.smach@dicella.com>
2022-05-12 19:11:05 +0200

    heroku deployment

--- django_content_downloader/settings.py
+++ django_content_downloader/settings.py
@@ -73,6 +73,8 @@ WSGI_APPLICATION = 'django_content_downloader.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
