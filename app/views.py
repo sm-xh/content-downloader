640dafb1fd6f6bd0243de4382a3814115b0505b7
Piotr Smach <p.smach@dicella.com>
2022-05-22 00:33:17 +0200

    fixed inproper files directories

--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ from pytube import *
 import ffmpeg
 import subprocess
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 pytube.metadata.YouTubeMetadata
 video_url = ''
@@ -41,47 +42,49 @@ def download(request):
     metadata_status = request.POST.get('meta', 'off')
     vid = YouTube(video_url)
 
+    filespath = os.path.join(BASE_DIR, 'files\\')
+
     if metadata_status == 'on':
         metadata = {"title": vid.title, "author": vid.author, "description": vid.description,
                     "publish_date": str(vid.publish_date), "keywords": vid.keywords, "length": vid.length,
                     "views": vid.views}
         # Writing to file
         filename = 'metadata.json'
-        filepath = "app\\files\\" + filename
-        with open(filename+filepath, "w") as file1:
+        with open(filespath+filename, "w") as file1:
             # Writing data to a file
             file1.write(str(metadata))
 
         # Open the file for reading content
-        path = open(filepath, 'r')
-        # Set the mime type
-        mime_type, _ = mimetypes.guess_type(filepath)
+        path = open(filespath+filename, 'r')
         # Set the return value of the HttpResponse
-        response = HttpResponse(path, content_type=mime_type)
+        response = HttpResponse(path, content_type="application/json")
         # Set the HTTP header for sending to browser
         response['Content-Disposition'] = "attachment; filename=%s" % filename
         path.close()
-        os.remove(filepath)
+        os.remove(filespath+filename)
+
+        # todo: change metadata to seperate download button
+        return response
 
     if file_format == "Video":
         resolution = request.POST['resolution']
-        vid.streams.filter(res=resolution).first().download("files")
+        vid.streams.filter(res=resolution).first().download(filespath)
         filename = vid.title + ".mp4"
 
     elif file_format == "Audio":
-        vid.streams.filter(only_audio=True).first().download("files")
+        vid.streams.filter(only_audio=True).first().download(filespath)
         # running ffmpeg on downloaded file
-        filename_mp4 = 'app\\files\\'+vid.title+".mp4"
-        filename_mp3 = 'app\\files\\'+vid.title+".mp3"
+        filename_mp4 = filespath+vid.title+".mp4"
+        filename_mp3 = filespath+vid.title+".mp3"
         cmd = "ffmpeg -i {} -vn {}".format(filename_mp4, filename_mp3)
         subprocess.call("ffmpeg -i {} -vn {}".format(filename_mp4, filename_mp3))
         # changing filename
-        p = Path('files\\'+vid.title+'.mp4')
+        p = Path(filespath+vid.title+'.mp4')
         p.rename(p.with_suffix('.mp3'))
         filename = vid.title + ".mp3"
 
-    #mime_type, _ = mimetypes.guess_type("files\\"+filename)
-    path = open("app\\files\\"+filename, 'rb')
+    #mime_type, _ = mimetypes.guess_type("files\\"+filespath)
+    path = open(filespath+filename, 'rb')
     # Set the return value of the HttpResponse
     if file_format == "Video":
         response = HttpResponse(path, content_type="video/mp4")
@@ -90,5 +93,5 @@ def download(request):
     # Set the HTTP header for sending to browser
     response['Content-Disposition'] = "attachment; filename=%s" % filename
     path.close()
-    os.remove("app\\files\\"+filename)
+    os.remove(filespath+filename)
     return response
