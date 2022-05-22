87347db455bfeb13785bffc9279a561995f2d129
Piotr Smach <p.smach@dicella.com>
2022-05-22 22:42:12 +0200

    improved metadata download option

--- app/views.py
+++ app/views.py
@@ -5,7 +5,10 @@ import mimetypes
 import os
 import pytube.metadata
 from pathlib import Path
+from os.path import basename
 from pytube import *
+from zipfile import ZipFile
+
 import ffmpeg
 import subprocess
 
@@ -44,28 +47,7 @@ def download(request):
 
     filespath = os.path.join(BASE_DIR, 'files/')
 
-    if metadata_status == 'on':
-        metadata = {"title": vid.title, "author": vid.author, "description": vid.description,
-                    "publish_date": str(vid.publish_date), "keywords": vid.keywords, "length": vid.length,
-                    "views": vid.views}
-        # Writing to file
-        filename = 'metadata.json'
-        with open(filespath+filename, "w") as file1:
-            # Writing data to a file
-            file1.write(str(metadata))
-
-        # Open the file for reading content
-        path = open(filespath+filename, 'r')
-        # Set the return value of the HttpResponse
-        response = HttpResponse(path, content_type="application/json")
-        # Set the HTTP header for sending to browser
-        response['Content-Disposition'] = "attachment; filename=%s" % filename
-        path.close()
-        os.remove(filespath+filename)
-
-        # todo: change metadata to seperate download button
-        return response
-
+    # section for downloading the
     if file_format == "Video":
         resolution = request.POST['resolution']
         vid.streams.filter(res=resolution).first().download(filespath)
@@ -77,21 +59,54 @@ def download(request):
         filename_mp4 = filespath+vid.title+".mp4"
         filename_mp3 = filespath+vid.title+".mp3"
         cmd = "ffmpeg -i {} -vn {}".format(filename_mp4, filename_mp3)
-        subprocess.call("ffmpeg -i {} -vn {}".format(filename_mp4, filename_mp3))
+        subprocess.call(cmd)
         # changing filename
         p = Path(filespath+vid.title+'.mp4')
         p.rename(p.with_suffix('.mp3'))
         filename = vid.title + ".mp3"
 
-    #mime_type, _ = mimetypes.guess_type("files\\"+filespath)
-    path = open(filespath+filename, 'rb')
-    # Set the return value of the HttpResponse
-    if file_format == "Video":
-        response = HttpResponse(path, content_type="video/mp4")
-    elif file_format == "Audio":
-        response = HttpResponse(path, content_type="audio/mpeg")
-    # Set the HTTP header for sending to browser
-    response['Content-Disposition'] = "attachment; filename=%s" % filename
-    path.close()
-    os.remove(filespath+filename)
-    return response
+    if metadata_status == 'on':
+        metadata = {"title": vid.title, "author": vid.author, "description": vid.description,
+                    "publish_date": str(vid.publish_date), "keywords": vid.keywords, "length": vid.length,
+                    "views": vid.views}
+        # Writing to file
+        meta_name = 'metadata.json'
+        with open(filespath + meta_name, "w") as file1:
+            # Writing data to a file
+            file1.write(str(metadata))
+
+        zip_name = 'content.zip'
+        zipObj = ZipFile(filespath + zip_name, 'w')
+
+        # Add multiple files to the zip
+        zipObj.write(filespath + meta_name, basename(filespath + meta_name))
+        zipObj.write(filespath + filename, basename(filespath + filename))
+        zipObj.close()
+
+        # Open the file for reading content
+        path = open(filespath+'content.zip', 'rb')
+        # Set the return value of the HttpResponse
+        response = HttpResponse(path, content_type='application/x-zip-compressed')
+
+        # Set the HTTP header for sending to browser
+        response['Content-Disposition'] = "attachment; filename=%s" % zip_name
+
+        path.close()
+        os.remove(filespath + meta_name)
+        os.remove(filespath + zip_name)
+        os.remove(filespath + filename)
+
+        # todo: change metadata to seperate download button
+        return response
+    else:
+        path = open(filespath+filename, 'rb')
+        # Set the return value of the HttpResponse
+        if file_format == "Video":
+            response = HttpResponse(path, content_type="video/mp4")
+        elif file_format == "Audio":
+            response = HttpResponse(path, content_type="audio/mpeg")
+        # Set the HTTP header for sending to browser
+        response['Content-Disposition'] = "attachment; filename=%s" % filename
+        path.close()
+        os.remove(filespath+filename)
+        return response
