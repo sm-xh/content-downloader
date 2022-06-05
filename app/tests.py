8e44211515f0cf994fe38aea9a3b917762f69fca
Piotr Smach <p.smach@dicella.com>
2022-06-05 23:50:03 +0200

    deleted e2e test

--- app/tests.py
+++ app/tests.py
@@ -209,17 +209,17 @@ class TestDownloadView(TestCase):
             "attachment; filename=content.zip"
         )
 
-
-class EndToEndTest(TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-
-    def test_signup_fire(self):
-        self.driver.get("http://localhost:8000/add/")
-        self.driver.find_element_by_id('id_title').send_keys("test title")
-        self.driver.find_element_by_id('id_body').send_keys("test body")
-        self.driver.find_element_by_id('submit').click()
-        self.assertIn("http://localhost:8000/", self.driver.current_url)
-
-    def tearDown(self):
-        self.driver.quit
+#
+# class EndToEndTest(TestCase):
+#     def setUp(self):
+#         self.driver = webdriver.Firefox()
+#
+#     def test_signup_fire(self):
+#         self.driver.get("http://localhost:8000/add/")
+#         self.driver.find_element_by_id('id_title').send_keys("test title")
+#         self.driver.find_element_by_id('id_body').send_keys("test body")
+#         self.driver.find_element_by_id('submit').click()
+#         self.assertIn("http://localhost:8000/", self.driver.current_url)
+#
+#     def tearDown(self):
+#         self.driver.quit
